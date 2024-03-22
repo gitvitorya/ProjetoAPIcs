c4334b52240a31a80b418e8e80ebed0e4f3c8e56
gitvitorya <124717307+gitvitorya@users.noreply.github.com>
2024-03-22 11:37:41 -0300

    Subindo projeto

---
+++ ProjetoAPIcs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ProjetoAPIcs {
+    class Program {
+        static void Main(string[] args) {
+
+            var requisicao = WebRequest.CreateHttp("https://jsonplaceholder.typicode.com/todos");
+            requisicao.Method = "GET"; // DEFINE O METODO DA REQUISICAO
+
+            //resposta armazena a resposta da requisicao
+            using (var resposta = requisicao.GetResponse()) {
+
+                //decodificando
+                var stream = resposta.GetResponseStream(); 
+                StreamReader leitor = new StreamReader(stream);
+
+                object dados = leitor.ReadToEnd();
+
+                //converte os dados para lista de Tarefas
+                List<Tarefa> tarefas = JsonConvert.DeserializeObject<List<Tarefa>>(dados.ToString());
+
+                foreach(Tarefa tarefa in tarefas) {
+
+                    tarefa.Exibir();
+
+                }
+
+                stream.Close(); //toda stream criada deve ser fechada
+                resposta.Close();
+
+              //  Console.WriteLine(dados.ToString()); //converte o objeto em string
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
---
+++ ProjetoAPIcs/Tarefa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAPIcs {
+    class Tarefa {
+
+        //necessario mesmo nome que na api
+        public int userId;
+        public int id;
+        public string title;
+        public bool completed;
+
+        public void Exibir() {
+
+            Console.WriteLine("Objeto tarefa");
+            Console.WriteLine($"UserId: {userId}");
+            Console.WriteLine($"Id: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Finalizou?: {completed}");
+            Console.WriteLine("------------------------------------------------");
+
+        }
+
+    }
+}
